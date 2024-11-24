775eee433ffdbe654022e2fc422890af086fe2a3
evandro.albuquerque <evandro.albuquerque@codata.pb.gov.br>
2024-11-23 21:34:42 -0300

    Implementa projeto completo.

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './cart/reducer';
+
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
+
+export default store;
